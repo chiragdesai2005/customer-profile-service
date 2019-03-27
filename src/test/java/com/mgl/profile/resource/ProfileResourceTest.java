f6c2635ba09942e689b19582cddd6c9c174218b4
chirag <chiragdesai2005@gmail.com>
2019-03-27 22:29:19 +1100

    Added sample test case using resttemplate

--- src/test/java/com/mgl/profile/resource/ProfileResourceTest.java
+++ src/test/java/com/mgl/profile/resource/ProfileResourceTest.java
@@ -1,20 +1,39 @@
 package com.mgl.profile.resource;
 
+import com.google.gson.Gson;
+import com.mgl.profile.exception.ErrorDetails;
+import com.mgl.profile.model.JwtUser;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpMethod.GET;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
 @AutoConfigureWebTestClient
 @RunWith(SpringRunner.class)
 public class ProfileResourceTest {
 
     @Autowired
     private WebTestClient webClient;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private Gson gson;
+
 
     @Test
     /**
@@ -26,8 +45,33 @@ public class ProfileResourceTest {
     }
 
     @Test
+    /**
+     *  Test case - API call with Jwt
+     *  Expect 404, Record not found
+     */
     public void testGetProfile() {
         // 1 - get JWT token and call get API
+        HttpEntity<JwtUser> request = new HttpEntity<>(new JwtUser(123L,"chirag","admin"));
+        String token = restTemplate.postForObject("/v1/token", request, String.class);
+        System.out.println("------------------------");
+        System.out.println(token);
+        System.out.println("------------------------");
+        assertThat(token).isNotNull();
         // 2 - validate response and Http status code
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer "+token);
+        headers.set("Content-Type", "application/json");
+        HttpEntity entity = new HttpEntity(headers);
+        ResponseEntity<String> response = restTemplate.exchange("/v1/profile/1", GET,entity,String.class);
+        String body = response.getBody();
+        assertThat(body).isNotNull();
+        System.out.println("-------------------------");
+        System.out.println(response);
+        System.out.println("-------------------------");
+        ErrorDetails errorDetails = gson.fromJson(body, ErrorDetails.class);
+        assertThat(errorDetails).isNotNull();
+        assertThat(errorDetails.getMessage()).isNotNull();
+        assertThat(errorDetails.getMessage()).isEqualTo("Record not found for given id 1");
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
     }
 }
